fbb01209bcaaf47c8824e4e1aebc5b5a97f4a9ea
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2022-02-15 14:10:41 -0300

    bugfix: moving subfolders with files and some logic errors

--- Sort.py
+++ Sort.py
@@ -1,19 +1,34 @@
 import os
+import sys
 
-def ordenary(letter, Dir, nameProgram):
+def subSort(copyFold, originFold):
+    listFold = os.listdir(originFold)
+    for file in listFold:
+            if os.path.isdir(originFold+"/"+file):
+                os.makedirs(copyFold + "/" + file)
+                subSort(copyFold + "/" + file, originFold + "/" + file)
+            else:
+                archive = open(copyFold + "/" + file, 'w')
+                archive.close()
+                os.rename(originFold + "/" + file, copyFold + "/" + file)
+
+
+def Sort(letter, Dir, nameProgram):
     listDir = os.listdir(Dir)
     for file in listDir:
         bletter = (file[0] == letter or file[0] == letter.upper()) and file != nameProgram
-        if bletter and not(os.path.isdir(Dir+"/"+letter.upper()) and file == letter.upper()):
-            try:
-                print(f"Creating folder {letter}..")
-                os.makedirs(letter.upper(), exist_ok=True)
-                print(f"Folder {letter} created")
-            except:
-                None
-            archive = open(Dir + "/" + letter.upper() + "/" + file)
-            archive.close()
-            os.rename(Dir + "/" + file, Dir + "/" + letter.upper() + "/" + file)
+        if bletter and file != letter.upper():
+            print(f"Creating folder {letter}..")
+            os.makedirs(letter.upper(), exist_ok=True)
+            print(f"Folder {letter} created")
+            
+            if os.path.isdir(Dir+"/"+file):
+                os.makedirs(Dir + "/" + letter.upper() + "/" + file)
+                subSort(Dir + "/" + letter.upper() + "/" + file, Dir + "/" + file)
+            else:
+                archive = open(Dir + "/" + letter.upper() + "/" + file, 'w')
+                archive.close()
+                os.rename(Dir + "/" + file, Dir + "/" + letter.upper() + "/" + file)
 
 
 print("Running..")
@@ -22,4 +37,4 @@ ListAlphabet = [chr(i) for i in range(ord('a'),ord('z')+1)]
 nameProgram = os.path.basename(__file__)
 for letter in ListAlphabet:
     print(f"Sorting folders and/or files with the letter {letter}..")
-    ordenary(letter,Dir, nameProgram)
+    Sort(letter,Dir, nameProgram)
