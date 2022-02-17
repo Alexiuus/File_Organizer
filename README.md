5c296b6d44dffc18894589119ac9f04ef5f84d08
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2022-02-17 13:22:00 -0300

    New implementation: Help command and data encapsulation

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # File organizer for Linux
 ## Resume
+
 Program that sort folders and files in the directories given as argument.
+
 ## Functioning
-Run the program giving as argument the directories to be sorted (the directory address must start from the desktop/Escritorio).
-## Example:
-    ```
-    user@user-BB-666:~/Desktop/prog/example$ python3 Sort.py Desktop/folder/example1 Desktop/folder/example2 Desktop/folder/example3
-    ```
-    
-    
+
+Run the program giving as argument the directories to be sorted (the directory address must start from the desktop/Escritorio). for more help, run:
+
+```
+    python3 Sort.py
+```
+
+or:
+
+```
+    python3 Sort.py --help
+```
+
+in the CMD.
--- Sort.py
+++ Sort.py
@@ -1,6 +1,30 @@
 import os
 import sys
 
+class Data:
+    def __init__(self):
+        self.__noSort = [os.path.basename(__file__), "README.md"]
+        self.__ListAlphabet = [chr(i) for i in range(ord('a'),ord('z')+1)]
+        self.__argvs = sys.argv
+
+    def getNoSort(self):
+        return self.__noSort
+    
+    def getListAlphabet(self):
+        return self.__ListAlphabet
+
+    def getArgv(self):
+        return self.__argvs
+
+    def isNoSort(self,file):
+        for elem in self.__noSort:
+            if elem==file:
+                return True
+        for elem in self.__ListAlphabet:
+            if elem==file:
+                return True
+        return False
+
 def subSort(copyFold, originFold):
     listFold = os.listdir(originFold)
     for file in listFold:
@@ -14,16 +38,16 @@ def subSort(copyFold, originFold):
     os.rmdir(originFold) 
 
 
-def Sort(letter, Dir, nameProg):
+def Sort(letter, Dir, data):
     listDir = os.listdir(Dir)
     for file in listDir:
         if ((file[0]==letter or file[0]==letter.upper()) and 
-            file!=nameProg and file!=letter.upper()):
-            if not(os.path.isdir(Dir+"/"+letter.upper())):
+            not(data.isNoSort(file))):
+            if not(os.path.isdir(Dir + "/" + letter.upper())):
                 print(f"Creating folder {letter}..")
-                os.makedirs(Dir+"/"+letter.upper())
+                os.makedirs(Dir+ "/" + letter.upper())
                 print(f"Folder {letter} created")
-            if os.path.isdir(Dir+"/"+file):
+            if os.path.isdir(Dir + "/" + file):
                 os.makedirs(Dir + "/" + letter.upper() + "/" + file)
                 subSort(Dir + "/" + letter.upper() + "/" + file, Dir + "/" + file)
             else:
@@ -31,27 +55,43 @@ def Sort(letter, Dir, nameProg):
                 archive.close()
                 os.rename(Dir + "/" + file, Dir + "/" + letter.upper() + "/" + file)
 
-print("Running..")
-argv = sys.argv
-if (len(argv)<2):
-    sys.exit("[ERROR] No directories to sort")
+def run(data):
+    print("Running..")
+    while 1:
+        if (os.path.isdir(f"Escritorio") or os.path.isdir(f"Desktop")):
+            break
+        elif(os.getcwd()=="/"):
+            sys.exit("[ERROR] Desktop not found")
+        os.chdir("../")
 
-while 1:
-    if (os.path.isdir(f"Escritorio") or os.path.isdir(f"Desktop")):
-        break
-    elif(os.getcwd()=="/"):
-        sys.exit("[ERROR] Desktop not found")
-    os.chdir("../")
+    Dir = os.getcwd()
 
-Dir = os.getcwd()
-ListAlphabet = [chr(i) for i in range(ord('a'),ord('z')+1)]
-nameProg = os.path.basename(__file__)
+    for i in range(1,len(data.getArgv())):
+        if not(os.path.isdir(data.getArgv()[i])):
+            sys.exit(f"[ERROR] Does not exist the Directory: {data.getArgv()[i]}")
+        print(f"[SORTING]: {data.getArgv()[i]}")
+        for letter in data.getListAlphabet():
+            print(f"Sorting folders and/or files with the letter {letter}..")
+            Sort(letter,Dir + "/" + data.getArgv()[i], data)
+    print("[FINALIZED]")
 
-for i in range(1,len(argv)):
-    if not(os.path.isdir(argv[i])):
-        sys.exit(f"[ERROR] Does not exist the Directory: {argv[i]}")
-    print(f"[Sorting]: {argv[i]}")
-    for letter in ListAlphabet:
-        print(f"Sorting folders and/or files with the letter {letter}..")
-        Sort(letter,Dir+"/"+argv[i], nameProg)
-print("[Finalized]")
+def help():
+    print("Running mode:\n"
+            "[STANDARD]:\n"
+            "   python3 Sort.py \"<directory1>\" \"<directory2>\" ... \"<directoryN>\"\n"
+            "Commands:\n"
+            "[HELP]:\n"
+            "   python3 Sort.py --help\n"
+            "   python3 Sort.py\n")
+
+data = Data()
+
+if len(data.getArgv())<=2:
+    if len(data.getArgv())<=1 or data.getArgv()[1]=='--help':
+        help()
+    elif data.getArgv()[1]!='--help':
+        run(data)
+    else:
+        sys.exit("[ERROR] Invalid arguments")
+else:
+    run(data)
